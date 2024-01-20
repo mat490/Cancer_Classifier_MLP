afb6a6c7dac276bd9cda7b2c94d653f73610ee56
Matías Alejandro Suárez Zúñiga <matsuar@hotmail.com>
2024-01-20 16:29:55 -0600

    Update MLP_Cells.py

--- MLP_Cells.py
+++ MLP_Cells.py
@@ -7,22 +7,22 @@ import pandas as pd
 
 df_cancer_cells = pd.read_csv('/Cancer_Data.csv')
 
-# Convertimos la columna "diagnosis" de df_cancer_cells en valores numericos cambiando las "M" por 1 y las "B" por 0
+# Convert the "diagnosis" column of df_cancer_cells into numerical values by changing the "M" to 1 and the "B" to 0
 df_cancer_cells['diagnosis'].replace(['M','B'],[1,0],inplace=True)
 
-# Separamos nuestras entradas y nuestras salidas
+# Separate our entrances and our exits
 x_df_cc = df_cancer_cells.iloc[:,2:]
 y_df_cc = df_cancer_cells['diagnosis']
 
-# En nuestra muestra separamos nuestros datos de entrenamiento y nuestros datos de salida
+# In our sample we separate our training data and our output data
 X_train, X_test, y_train, y_test = train_test_split(x_df_cc, y_df_cc, test_size=0.2, random_state=42)
 
-# Crear el modelo
+# Create the model
 model = Sequential()
 model.add(Dense(units= 12, activation='relu', input_dim=30))  # Ajusta input_dim al número de características
 model.add(Dense(units=1, activation='sigmoid'))
 
-# Compilamos el modelo
+# Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Entrenamos el modelo
@@ -31,13 +31,13 @@ entrenar_modelo()
 def entrenar_modelo():
   model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
   
-  # Verificamos la presición
+  # Check accuracy
   test_loss, test_accuracy = model.evaluate(X_test, y_test)
   print(f'\nTest Accuracy: {test_accuracy*100:.2f}%')
   
-  # Guardamos el modelo si la presición es mayor o igual a 0.95
+  # Save the model if the pressure is greater than or equal to 0.95
   if (test_accuracy >= 0.95):
   
-    model.save('modelo_preentrenado_cc.h5')
+    model.save('model_pretrained_bcc.h5')
   else:
     entrenar_modelo()
